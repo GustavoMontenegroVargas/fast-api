ec47694c2d09a8faa4bce66b5decbb9f9c49bf40
GustavoMontenegroVargas <c.montenegro2402@gmail.com>
2026-02-25 18:37:08 -0600

    Ejemplos con groupby

---
+++ src/pandas_test.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+# 1. Creamos el dataset de ejemplo
+data = {
+    'Vendedor': ['Ana', 'Ana', 'Ana', 'Beto', 'Beto', 'Carlos', 'Carlos'],
+    'Monto_Venta': [100, 150, 600, 200, 250, 300, 350]
+}
+
+print(data)
+print(data.keys())
